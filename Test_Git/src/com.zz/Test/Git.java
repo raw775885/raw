d553cc3d88b4ed188da4a0e27d30b822142deff7
raw775885 <z1281933702@icloud.com>
2022-04-05 18:07:00 +0800

    初中生

---
+++ Test_Git/src/com.zz/Test/Git.java
@@ -0,0 +1,15 @@
+package com.zz.Test;
+
+/**
+ * @Descriptions:
+ * @Author: ZeZhao
+ * @CreateDate: 2022/4/5 18:02
+ * @Version: 1.0
+ */
+public class Git {
+
+    public static void main(String[] args) {
+        System.out.println("曹泽晟呆逼");
+    }
+
+}
